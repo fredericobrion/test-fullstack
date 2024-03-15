631912d38fd2ebf49528c51acd467f8803b39599
Frederico Brion <fredericoasbrion@gmail.com>
2024-03-15 18:41:47 -0300

    feat: access-control-allow-origins

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -12,7 +12,7 @@ class App {
 
   private config(): void {
     const accessControl: express.RequestHandler = (_req, res, next) => {
-      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Origin", "http://localhost:5173");
       res.header("Access-Control-Allow-Methods", "GET, POST, PUT");
       res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
       next();
