9dd1d1b69bb14ee11fd4d5e2d7df6aaa5ee063e3
Frederico Brion <fredericoasbrion@gmail.com>
2024-03-15 17:49:44 -0300

    feat: css main page

--- front-end/src/components/userCard/index.tsx
+++ front-end/src/components/userCard/index.tsx
@@ -1,28 +1,45 @@
 import { User } from "../../types/User";
 import styles from "./userCard.module.css";
+import { useNavigate } from "react-router-dom";
 
 type UserCardProps = {
   user: User;
 };
 
 function UserCard({ user }: UserCardProps) {
+  const navigate = useNavigate();
+
   const { cpf, email, id, name, phone, status } = user;
+
+  const statusColor = (status: string) => {
+    switch (status) {
+      case "ACTIVE":
+        return styles.activeCircle;
+      case "INACTIVE":
+        return styles.inactiveCircle;
+      case "PENDING":
+        return styles.pendingCircle;
+      default:
+        return styles.disabledCircle;
+    }
+  }
 
   return (
     <div className={styles.container}>
       <div className={styles.col1}>
-        <p>{name}</p>
+        <p><strong>{name}</strong></p>
         <p>{email}</p>
       </div>
       <div className={styles.col2}>
+        <p><strong>{cpf}</strong></p>
         <p>{phone}</p>
-        <p>{cpf}</p>
       </div>
       <div className={styles.col3}>
+        <div className={statusColor(status)}></div>
         <p>{status}</p>
       </div>
       <div className={styles.col4}>
-        <button>Editar</button>
+        <button onClick={() => navigate(`${id}`)}>Editar</button>
       </div>
     </div>
   );
--- front-end/src/pages/main/index.tsx
+++ front-end/src/pages/main/index.tsx
@@ -1,22 +1,41 @@
-import { useContext, useEffect } from 'react';
-import Context from '../../context/Context';
-import UserCard from '../../components/userCard';
+import { useContext, useEffect } from "react";
+import Context from "../../context/Context";
+import UserCard from "../../components/userCard";
+import userIcon from "../../assets/user-icon.svg";
+import styles from './main.module.css';
+import { useNavigate } from "react-router-dom";
 
 function MainPage() {
-  const {getUsersFromDb, users} = useContext(Context);
+  const navigate = useNavigate();
+
+  const { getUsersFromDb, users } = useContext(Context);
 
   useEffect(() => {
     if (!users.length) {
       getUsersFromDb();
     }
-  }, [])
+  }, []);
 
   return (
-    <div>
-      {users.map((user) => {
-        return <UserCard key={user.id} user={user}/>
-      })}
-    </div>
+    <>
+      <h1 className={styles.title}>
+        <img src={userIcon} alt="Logo de usuário" />
+        Painel de clientes
+      </h1>
+      <div className={styles.container}>
+        <div>
+          <h2>Listagem de usuários</h2>
+          <h3>Escolha um cliente para visualizar os detalhes</h3>
+        </div>
+        <button onClick={() => navigate('add')}>Novo cliente</button>
+      </div>
+      <div>
+        {users.map((user) => {
+          return <UserCard key={user.id} user={user} />;
+        })}
+      </div>
+      <p className={styles.clientsCount}>Exibindo {users.length} clientes</p>
+    </>
   );
 }
 
