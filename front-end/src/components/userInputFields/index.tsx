28a7ef6a4f8bf038270d08f99f86daf3dba287d4
Frederico Brion <fredericoasbrion@gmail.com>
2024-03-16 08:21:08 -0300

    feat: cpf and phone formatter

--- front-end/src/components/userInputFields/index.tsx
+++ front-end/src/components/userInputFields/index.tsx
@@ -1,9 +1,12 @@
-import { useForm } from "react-hook-form";
-import { User } from "../../types/User";
+import { useState } from "react";
 import styles from "./userInputFields.module.css";
 import { useNavigate, useLocation } from "react-router-dom";
+import { formatCPF, formatPhone } from "../../utils/formatInputs";
 
 function UserInputFields() {
+  const [cpf, setCpf] = useState("");
+  const [phone, setPhone] = useState("");
+
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -11,8 +14,20 @@ function UserInputFields() {
     <form className={styles.form}>
       <input type="text" id="name" placeholder="Nome" />
       <input type="email" id="email" placeholder="E-mail" />
-      <input type="text" id="cpf" placeholder="CPF" />
-      <input type="text" id="phone" placeholder="Telefone" />
+      <input
+        type="text"
+        id="cpf"
+        placeholder="CPF"
+        value={cpf}
+        onChange={(e) => setCpf(formatCPF(e.target.value))}
+      />
+      <input
+        type="text"
+        id="phone"
+        placeholder="Telefone"
+        value={phone}
+        onChange={(e) => setPhone(formatPhone(e.target.value))}
+      />
       <select id="status">
         <option value="ACTIVE">Active</option>
         <option value="INACTIVE">Inactive</option>
@@ -20,7 +35,9 @@ function UserInputFields() {
         <option value="PENDING">Pending</option>
       </select>
       <div>
-        <button type="submit">{location.pathname === '/add' ? "Criar" : "Atualizar"}</button>
+        <button type="submit">
+          {location.pathname === "/add" ? "Criar" : "Atualizar"}
+        </button>
         <button onClick={() => navigate("/")}>Voltar</button>
       </div>
     </form>
